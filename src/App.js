e7c8b1da67bbb389b1f2563ecee35638ac5e9387
FalixRe <wojtunieczek@o2.pl>
2023-05-31 21:28:13 +0200

    build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,148 @@
-import logo from './logo.svg';
+
+
+import React from 'react';
+import { createStore } from 'redux'
+//import logo from './logo.svg';
+//import ship from './images/drawing.svg';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
 import './App.css';
+
+class Point {
+  constructor(coordX, coordY) {
+    this.coordX = coordX;
+    this.coordY = coordY;
+  }
+
+  move(pathX, pathY) {
+    this.coordX += pathX;
+    this.coordY += pathY;
+    return this;
+  }
+}
+
+// function counterReducer(state = { value: 0 }, action) {
+//   switch (action.type) {
+//     case 'counter/incremented':
+//       return { value: state.value + 1 }
+//     case 'counter/decremented':
+//       return { value: state.value - 1 }
+//     default:
+//       return state
+//   }
+// }
+
+// // Create a Redux store holding the state of your app.
+// // Its API is { subscribe, dispatch, getState }.
+// let store = createStore(counterReducer)
+
+export const store = createStore(reducers); //to see how to use
+
+
+class SeaBackground extends React.Component {
+
+  state = {
+    position: new Point(0,0)
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        position: this.state.position.move(1,1)
+      })
+      console.log("pozycja"+this.state.position);
+    }, 10);
+  }
+
+  render() {
+    return(
+      <g id="sea" >
+        <defs>
+          <pattern id="buoysPatterns" viewBox="0,0,500,500" width="500" height="500" patternUnits="userSpaceOnUse">
+            <circle
+            fill="#ffdf12"
+            stroke="black"
+            id="paternBuoy"
+            cx={250}
+            cy={250}
+            r="10" 
+            />
+          </pattern>
+        </defs>
+        <rect
+          fill="#60c4e6"
+          id="seaColor"
+          width="2000"
+          height="2000"
+          x="-500"
+          y="-500" 
+        />
+        <rect
+          style={{
+            fill: "url(#buoysPatterns)"
+          }}
+          id="buoys"
+          width="2000"
+          height="2000"
+          x={-500}
+          y={-500}
+          transform={"matrix(1,0,0,1,"+this.state.position.coordX%500+","+this.state.position.coordY%500+")"}
+        />
+      </g>
+    )
+    }
+}
+
+class Boat extends React.Component {
+  render() {
+    return(
+      <g>
+        <rect
+        style={{
+          fill: "#ffa600",
+          stroke: "black"
+        }}
+        id="s"
+        width="40"
+        height="80"
+        x="500"
+        y="500"
+        transform={"matrix(1,0,0,1,0,0"}
+        />
+      </g>
+    )
+  }
+}
+
+function EnginePowerInput() {
+  return(
+    <input type='number' name="enginePower">
+
+    </input>
+  )
+}
+
+function Course() {
+  return(
+
+    <input type='number'  name="boatCourse">
+      
+    </input>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Provider store={store}>
+        <EnginePowerInput />
+        <Course />
+        <svg id="shipAplet" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 1000 1000">
+          <SeaBackground/>
+          <Boat/>
+        </svg>
+        </Provider>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +152,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Pfreact
         </a>
       </header>
     </div>
